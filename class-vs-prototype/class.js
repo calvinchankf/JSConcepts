9fc0ad7752f93dc86a78404a1d53e6b14247ab5f
calvinchankf <chan9118kin@gmail.com>
2018-12-20 14:18:58 +0800

    class vs prototype

---
+++ class-vs-prototype/class.js
@@ -0,0 +1,66 @@
+// declaration
+class Vehicle {
+  constructor(make, model, color) {
+    this.make = make;
+    this.model = model;
+    this.color = color;
+  }
+
+  // instance method: need an instance, every instance has its own props
+  getName() {
+    return this.make + " " + this.model;
+  }
+
+  // static/class method: dont need to new an instance
+  static getIntro() {
+    return "intro";
+  }
+}
+
+// much more OOP than protytype
+let car = new Vehicle("Toyota", "Corolla", "Black");
+console.log(car);
+// console.log(car.getIntro()); // crash because getIntro() is a class method
+console.log(Vehicle.getIntro());
+
+// get/setter
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  // be careful: please use underscore _ , or it calls itself, the getter, recursively
+  get name() {
+    console.log(`get name ${this._name}`)
+    return this._name;
+  }
+
+  // be careful: please use underscore _ , or it calls itself, the setter, recursively
+  set name(value) {
+    console.log(`set name ${value}`)
+    this._name = value;
+  }
+}
+
+let calvin = new Person("calvin")
+calvin.name = 'Calvin Chan'
+console.log(calvin.name)
+
+// subclass
+class Car extends Vehicle {
+  // override
+  getName() {
+    return this.make + " " + this.model + " in child class.";
+  }
+}
+let car3 = new Car("Honda", "Accord", "Purple");
+console.log(car3.getName()); // "Honda Accord in child class."
+
+class Bus extends Vehicle {
+  // 'super' the method
+  getName() {
+    return super.getName() + " <- super !!!"
+  }
+}
+let car4 = new Bus("Honda", "Accord", "Purple");
+console.log(car4.getName()); // Honda Accord <- super !!!
---
+++ class-vs-prototype/prototype.js
@@ -0,0 +1,28 @@
+// declaration
+function Vehicle(make, model, color) {
+  this.make = make;
+  this.model = model;
+  this.color = color;
+  this.getName = function () {
+    return this.make + " " + this.model;
+  }
+}
+
+let car = new Vehicle("Toyota", "Corolla", "Black");
+console.log(car);
+
+// problem 1
+let car2 = new Vehicle("Benz", "ggsget", "White");
+car.year = 2012
+console.log(car); // car has 'year'
+console.log(car2); // but car2 instance doesn't have 'year'
+
+// problem 2
+// all instances share the same values of the new variable. in this case, all instances share 2 as 'month'
+Vehicle.prototype.month = 2
+console.log(car) // cant see month in the car object
+console.log(car2) // cant see month in the car2 object
+console.log(car.month); // 2012
+console.log(car2.month); // 2012 WTF
+
+// That's why we want class, make js more OOP

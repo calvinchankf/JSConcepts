e406f18261e41aefee480ec03e766f19c4102847
calvinchankf <chan9118kin@gmail.com>
2019-01-02 20:28:53 +0800

    immediate functions

--- self-invoking/index.js
+++ self-invoking/index.js
@@ -1,3 +1,7 @@
+// An immediate function is one that executes as soon as it is defined.
+// Creating an immediate function is simple: 
+// you add the open/close parentheses after the closing curly bracket, 
+// and then wrap the entire function in parentheses. That's it
 let temp = (function () {
   var x = "Hello!!"; // I will invoke myself
   return x

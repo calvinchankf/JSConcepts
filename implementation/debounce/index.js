fc71fd238ff4c63b8e3f1c5e085a42ebdc435509
calvinchankf <chan9118kin@gmail.com>
2020-06-11 18:56:38 +0800

    revise debouncing

--- implementation/debounce/index.js
+++ implementation/debounce/index.js
@@ -1,35 +1,32 @@
-const debounce = (f, delay) => {
-    let once = null
-    /*
-    this block is actually the block of
-    
+const debounce = (fn, delay) => {
+	// this block is a closure, it is executed when we declare the function const a
+	let once = null;
+	/*
+    the below block is the fn
     (i) => {
         console.log('hello ', i)
     }
-    
-    so we can get this, arguments here within the scope 
-    const context = this
-    const args = arguments
     */
-    return function () {
-        const context = this
-        const args = arguments
-        if (once != null) {
-            clearTimeout(once)
-        }
-        once = setTimeout(() => {
-            f.apply(null, args)
-        }, delay)
-    }
-}
+	return function () {
+		// arguments are the params that we pass in fn
+		const args = arguments;
+		if (once != null) {
+			clearTimeout(once);
+		}
+		once = setTimeout(() => {
+			// the this is the context of the parent closure
+			fn.apply(this, args);
+		}, delay);
+	};
+};
 
 const a = debounce((i) => {
-    console.log('hello', i)
-}, 1000)
+	console.log("hello", i, this);
+}, 1000);
 
-a(1)
-a(2)
-a(3)
-a(4)
-a(5)
+a(1);
+// a(2);
+// a(3);
+// a(4);
+// a(5);
 // the terminal will just print hello 5 after one second

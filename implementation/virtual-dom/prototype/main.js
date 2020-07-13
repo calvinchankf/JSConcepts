0663050e2d44537c6ac845d8c147813852d55e28
calvinchankf <chan9118kin@gmail.com>
2020-07-13 21:06:37 +0800

    bugfix

--- implementation/virtual-dom/prototype/main.js
+++ implementation/virtual-dom/prototype/main.js
@@ -1,5 +1,5 @@
 // this is a handy function for creating a node
-const h = (tag, props, ...children) => {
+const h = (tag, props = {}, children = []) => {
 	return { tag, props, children };
 };
 
@@ -9,9 +9,11 @@ const render = (node) => {
 		return document.createTextNode(node);
 	}
 	const $el = document.createElement(node.tag);
+	// attributes
 	for (const [k, v] of Object.entries(node.props)) {
 		$el.setAttribute(k, v);
 	}
+	// children
 	for (const child of node.children) {
 		const $child = render(child);
 		$el.appendChild($child);
@@ -19,13 +21,26 @@ const render = (node) => {
 	return $el;
 };
 
-// naive diff for tag change OR text change
+// naive diff functions
 function isTagChanged(node1, node2) {
 	return (
 		typeof node1 !== typeof node2 ||
 		(typeof node1 === "string" && node1 !== node2) ||
 		node1.tag !== node2.tag
 	);
+}
+function isPropsChanged(node1, node2) {
+	for (let key in node1.props) {
+		if (!(key in node2.props) || node1.props[key] !== node2.props[key]) {
+			return true;
+		}
+	}
+	for (let key in node2.props) {
+		if (!(key in node1.props) || node1.props[key] !== node2.props[key]) {
+			return true;
+		}
+	}
+	return false;
 }
 
 /*
@@ -37,20 +52,23 @@ function isTagChanged(node1, node2) {
     For simplicity, here we use the (2)
 */
 function updateElement($parent, oldNode, newNode, index = 0) {
+	console.log;
 	if (!oldNode) {
 		$parent.appendChild(render(newNode));
 	} else if (!newNode) {
 		$parent.removeChild($parent.childNodes[index]);
-	} else if (isTagChanged(newNode, oldNode)) {
+	} else if (isTagChanged(oldNode, newNode)) {
 		$parent.replaceChild(render(newNode), $parent.childNodes[index]);
-	} else if (newNode.tag) {
-		const oldLength = oldNode.children.length;
+	} else if (isPropsChanged(oldNode, newNode)) {
+		$parent.replaceChild(render(newNode), $parent.childNodes[index]);
+	} else if (newNode.children) {
+		const oldLength = oldNode.children.length || 0;
 		const newLength = newNode.children.length;
 		for (let i = 0; i < Math.max(oldLength, newLength); i++) {
 			updateElement(
 				$parent.childNodes[index],
-				newNode.children[i],
 				oldNode.children[i],
+				newNode.children[i],
 				i
 			);
 		}
@@ -59,38 +77,36 @@ function updateElement($parent, oldNode, newNode, index = 0) {
 
 // ---------------------------------------------------------------------
 
-let current = h(
-	"div",
-	{ class: "list" },
-	h("div", {}, "😉😉😉"),
-	h("div", {}, "random"),
-	h(
-		"img",
-		{ src: "https://media.giphy.com/media/C9x8gX02SnMIoAClXa/giphy.gif" },
-		"image"
-	)
-);
+let current = h("div", { class: "list" }, [
+	h("div", {}, ["😉😉😉"]),
+	h("div", {}, ["random"]),
+	h("div", {}, ["random props"]),
+	h("img", {
+		src: "https://media.giphy.com/media/C9x8gX02SnMIoAClXa/giphy.gif",
+	}),
+]);
 
 // $ means a Real DOM element
 const $root = document.getElementById("root");
 updateElement($root, undefined, current);
+
 // button to update
 const $reload = document.getElementById("refresh");
 $reload.addEventListener("click", () => {
-	const b = h(
-		"div",
-		{ class: "list" },
-		h("div", {}, "😉😉😉"),
-		h("div", {}, `random => ${String(Math.random() * 10)}`),
+	const b = h("div", { class: "list" }, [
+		h("div", {}, ["😉😉😉"]),
+		h("div", {}, [`random => ${String(Math.random() * 10)}`]),
 		h(
-			"img",
+			"div",
 			{
-				src:
-					"https://media.giphy.com/media/C9x8gX02SnMIoAClXa/giphy.gif",
+				random: `${String(Math.random() * 10)}`,
 			},
-			"image"
-		)
-	);
+			["random props"]
+		),
+		h("img", {
+			src: "https://media.giphy.com/media/C9x8gX02SnMIoAClXa/giphy.gif",
+		}),
+	]);
 	updateElement($root, current, b);
 	current = b;
 });

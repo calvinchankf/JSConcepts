528f541a92581527f6665605817674e1083ee49b
calvinchankf <chan9118kin@gmail.com>
2023-06-30 16:21:10 +0100

    clear all timeouts

--- implementation/clearAllTimeouts/index.js
+++ implementation/clearAllTimeouts/index.js
@@ -1,4 +1,12 @@
 // https://leetcode.com/discuss/interview-question/349283/Facebook-or-Phone-Screen-or-Implement-clearAllTimeout
+
+/*
+1st approach: 
+(X) Leaks memory when timers are cleared via clearTimeout but clearAllTimeouts is not invoked.
+(X) Does not work with strict mode.
+
+-----------------------------------
+
 const nativeSetTimeout = setTimeout;
 let allTimeouts = [];
 
@@ -22,3 +30,47 @@ creallAllTimeout();
 setTimeout(() => console.log("6"), 100);
 
 // terminal prints 6 only after 100ms
+*/
+
+/*
+	2nd approach
+	- put the 1st logic in a closure for security and avoid memory leak
+	- strict mode
+	- use global scope
+	*/
+	
+let context = typeof window !== 'undefined' ? window : Function('return this')();
+(function(global) {
+	'use strict';
+	const originalSetTimeout = global.setTimeout;
+	const originalClearTimeout = global.clearTimeout;
+	const hs = new Set();
+
+	global.setTimeout = (fn, t) => {
+		const tid = originalSetTimeout(() => {
+			hs.delete(tid)
+			fn();
+		}, t);
+		hs.add(tid)
+		return tid;
+	};
+
+	global.clearTimeout = tid => {
+		hs.delete(tid)
+		originalClearTimeout(tid);
+	};
+
+	global.clearAllTimeouts = () => {
+		for (let tid of hs) {
+			originalClearTimeout(tid);
+		}
+	};
+})(context)
+
+setTimeout(() => console.log("1"), 100);
+setTimeout(() => console.log("2"), 100);
+setTimeout(() => console.log("3"), 100);
+setTimeout(() => console.log("4"), 100);
+setTimeout(() => console.log("5"), 100);
+clearAllTimeouts();
+setTimeout(() => console.log("6"), 100);

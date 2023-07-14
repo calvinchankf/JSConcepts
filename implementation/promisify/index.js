c279483cde3d0ad266b0bedf80a72e5e3ffac76c
calvinchankf <chan9118kin@gmail.com>
2023-07-14 12:10:10 +0100

    promisify

--- implementation/promisify/index.js
+++ implementation/promisify/index.js
@@ -16,15 +16,21 @@ const promisify = fn => {
 };
 
 // test
-function do_something(a, b, callback) {
-    if (a < 0 || b < 0) {
-      const err = Error('a and b must be positive');
-      callback(err);
+function do_something(...args) {
+    const callback = args.pop()
+    if (args.filter(x => typeof x === 'number').length !== args.length) {
+        const err = new Error('every input must be a number');
+        callback(null, err);
     } else {
-      callback(a + b);
+        const total = args.reduce((acc, cur) => acc + cur, 0) // my do_something is to sum all inputs
+        callback(total);
     }
 }
 
 const promisified_do_something = promisify(do_something)
 
 promisified_do_something(100, 99).then((res) => console.log(res))
+
+promisified_do_something(1,10,100,1000,10000).then((res) => console.log(res))
+
+promisified_do_something('a',10,100,1000,10000).then((res) => console.log(res))

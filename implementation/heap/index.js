488055df0a3847ae0c8ecede3604e4141d6ffbb4
calvinchankf <chan9118kin@gmail.com>
2023-07-20 13:00:24 +0100

    update

--- implementation/heap/index.js
+++ implementation/heap/index.js
@@ -1,96 +1,60 @@
-class Heap {
-  constructor() {
-    this.arr = []
-  }
-
-  heapify(values) {
-    // O(nlogn) version
-    for (let i = 0; i < values.length; i++) {
-      this.heapPush(values[i])
+class MinHeap {
+    constructor() {
+        this.A = []
     }
-    /*
-      O(n) version
-      bottom-up
-      shift down from bottom to up such that we minimize the number of shift operations
-      n/2 + n/8 + n/16.... = O(n)
-    */
-    this.arr = values
-    const n = values.length
-    for (let i = Math.floor(n / 2); i >= 0; i--) {
-      this._shitUp(i)
+    heapPush(x) {
+        this.A.push(x)
+        this._shiftUp(this.A.length-1)
     }
-  }
-
-  heapPush(value) {
-    this.arr.push(value)
-    let curIdx = this.arr.length - 1
-    this._shiftDown(curIdx)
-  }
-
-  heapPop() {
-    const res = this.arr[0]
-    const last = this.arr.pop()
-    if (this.arr.length == 0) {
-      return res
+    heapPop() {
+        const n = this.A.length;
+        [this.A[n-1], this.A[0]] = [this.A[0], this.A[n-1]];
+        const minNode = this.A.pop()
+        this._shiftDown(0)
+        return minNode
     }
-    this.arr[0] = last
-    this._shitUp(0)
-    return res
-  }
-
-  // used by heapify, pop
-  _shitUp(fromIdx) {
-    let cur = fromIdx
-    while (cur < this.arr.length) {
-      const left = cur * 2 + 1
-      const right = cur * 2 + 2
-      if (left < this.arr.length && right < this.arr.length) {
-        if (this.arr[cur] < this.arr[left] && this.arr[cur] < this.arr[right]) {
-          break
-        } else if (this.arr[left] < this.arr[right]) {
-          const temp = this.arr[cur]
-          this.arr[cur] = this.arr[left]
-          this.arr[left] = temp
-          cur = left
-        } else {
-          const temp = this.arr[cur]
-          this.arr[cur] = this.arr[right]
-          this.arr[right] = temp
-          cur = right
+    _shiftUp(idx) {
+        const parent = Math.floor((idx - 1)/2)
+        if (parent >= 0 && this.A[idx] < this.A[parent]) {
+            [this.A[idx], this.A[parent]] = [this.A[parent], this.A[idx]];
+            this._shiftUp(parent)
         }
-      } else if (left < this.arr.length) {
-        if (this.arr[left] < this.arr[cur]) {
-          const temp = this.arr[cur]
-          this.arr[cur] = this.arr[left]
-          this.arr[left] = temp
-          cur = left
+    }
+    _shiftDown(idx) {
+        const children = [idx*2+1, idx*2+2]
+        let smallest = idx
+        for (let i of children) {
+            if (i < this.A.length && this.A[i] < this.A[smallest]) {
+                smallest = i
+            }
         }
-        break
-      } else {
-        break
-      }
+        if (smallest != idx) {
+            [this.A[smallest], this.A[idx]] = [this.A[idx], this.A[smallest]];
+            this._shiftDown(smallest)
+        }
     }
-  }
-
-  // used by push
-  _shiftDown(fromIdx) {
-    let curIdx = fromIdx
-    while (curIdx > 0) {
-      const parentIdx = Math.floor((curIdx - 1) / 2)
-      if (this.arr[parentIdx] > this.arr[curIdx]) {
-        const temp = this.arr[curIdx]
-        this.arr[curIdx] = this.arr[parentIdx]
-        this.arr[parentIdx] = temp
-        curIdx = parentIdx
-      } else {
-        break
-      }
+    heapify(values) {
+        // O(nlogn) version
+        // for (let i = 0; i < values.length; i++) {
+        //   this.heapPush(values[i])
+        // }
+        /*
+            O(n) version
+            bottom-up
+            shift down from bottom to up such that we minimize the number of shift operations
+            n/2 + n/8 + n/16.... = O(n)
+        */
+        this.A = values
+        const n = values.length
+        for (let i = Math.floor(n / 2); i >= 0; i--) {
+            this._shiftDown(i)
+        }
     }
-  }
 }
 
-// unique values
-let h = new Heap()
+
+console.log("--- insert() ---")
+let h = new MinHeap()
 h.heapPush(8)
 h.heapPush(9)
 h.heapPush(6)
@@ -98,13 +62,13 @@ h.heapPush(7)
 h.heapPush(4)
 h.heapPush(3)
 h.heapPush(5)
-console.log(h.arr)
-while (h.arr.length > 0) {
+console.log(h.A)
+while (h.A.length > 0) {
   console.log(h.heapPop())
 }
 
-// duplicate values
-h = new Heap()
+console.log("--- insert() with duplicate values ---")
+h = new MinHeap()
 h.heapPush(8)
 h.heapPush(8)
 h.heapPush(6)
@@ -112,37 +76,33 @@ h.heapPush(4)
 h.heapPush(4)
 h.heapPush(3)
 h.heapPush(5)
-console.log(h.arr)
-while (h.arr.length > 0) {
+console.log(h.A)
+while (h.A.length > 0) {
   console.log(h.heapPop())
 }
 
-console.log("-------------------------")
-
-// unique values
-h = new Heap()
+console.log("--- heapify() ---")
+h = new MinHeap()
 h.heapify([6, 4, 2, 8, 9, 5, 7, 3])
 let res = []
-while (h.arr.length > 0) {
+while (h.A.length > 0) {
   res.push(h.heapPop())
 }
 console.log(res)
 
-// duplicate values
-h = new Heap()
+console.log("--- heapify() with duplicates --- ")
+h = new MinHeap()
 h.heapify([6, 4, 2, 8, 9, 5, 7, 4])
 res = []
-while (h.arr.length > 0) {
+while (h.A.length > 0) {
   res.push(h.heapPop())
 }
 console.log(res)
 
-console.log("-------------------------")
-
-// Heap Sort
+console.log("--- HeapSort ---")
 function heapsort(values) {
   const res = []
-  const h = new Heap()
+  const h = new MinHeap()
   h.heapify(values)
   const n = values.length
   for (let i = 0; i < n; i++) {

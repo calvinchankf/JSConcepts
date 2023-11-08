587d51acb84a18127ad958fbb922f752b1f4864b
calvinchankf <chan9118kin@gmail.com>
2023-11-08 14:09:19 +0000

    update

--- react-widgets-implementation/react-debounce-vs-throttle/src/hooks/useThrottle.js
+++ react-widgets-implementation/react-debounce-vs-throttle/src/hooks/useThrottle.js
@@ -2,13 +2,13 @@ import { useState, useRef, useEffect } from 'react'
 
 export const useThrottle = (value, ms) => {
     const [throttledValue, setThrottledValue] = useState(value);
-    const restTime = useRef(0)
+    const expiredAt = useRef(0)
 
     useEffect(() => {
-        const delay = Math.max(0, restTime.current - Date.now())
+        const delay = Math.max(0, expiredAt.current - Date.now())
         const timeoutID = setTimeout(() => {
             setThrottledValue(value)
-            restTime.current = Date.now() + ms
+            expiredAt.current = Date.now() + ms
         }, delay);
         return () => clearTimeout(timeoutID)
     }, [value, ms]);
